e4d96658fa631fe1924fda0f232c2225343a0b4f
finalvalue <finalvalue@foxmail.com>
2020-05-07 15:49:45 +0800

    Update yfrobotbit.ts

--- yfrobotbit.ts
+++ yfrobotbit.ts
@@ -3,7 +3,7 @@
  * http://www.yfrobot.com
  * email: yfrobot@qq.com
  */
-// color: #2EAF79 #23A9F2 icon： ✣✤✥✦❉❃ஃஂ
-//% color=#2EAF79 icon="✣" block="YF_Microbit" 
+// color: #2EAF79 #23A9F2 icon： ✣✤✥✦❉❃ஃஂ icon="✣"
+//% color=#2EAF79  block="YF_Microbit" 
 //% category="YF_Microbit"
 namespace YFRobotBit {}
