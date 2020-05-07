2df5cc050b77a6b335cbf69dd5dc91e5ca0f9cd4
finalvalue <finalvalue@foxmail.com>
2020-05-07 17:04:55 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,7 +1,7 @@
 // tests go here; this will not be compiled when this package is used as an extension.
 YFRobotBit.connectLcd(39);
 YFRobotBit.showStringOnLcd1602("Hello world!", 0, 15);
-YFRobotBit.showStringOnLcd1602("Hello yfrobot!", 16, 15);
+YFRobotBit.showStringOnLcd1602("YFRobot & Micro:bit!", 16, 15);
 YFRobotBit.setLcdBacklight(LcdBacklight.On);
 const pos1602: number = YFRobotBit.position1602(LcdPosition1602.P0);
 const isLcdConnected: boolean = YFRobotBit.isLcdConnected();
